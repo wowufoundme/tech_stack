b7cbfeddd92173346d99eecbc591809d44f3cf06
Shubhkirti Sharma <shubhkirti.work@gmail.com>
2020-01-17 16:24:11 +0530

    Edit theme of List

--- src/components/LibraryItem.js
+++ src/components/LibraryItem.js
@@ -18,7 +18,7 @@ class LibraryItem extends Component {
         subtitleStyle={styles.subtitleStyle}
         bottomDivider
         leftAvatar={{ source: WEBPACK }}
-        chevron={{ color: '#1a237e' }}
+        chevron={{ name: 'caret-down', type: 'font-awesome', color: '#FFFFFF' }}
         pad={10}
         onPress={() => []}
         containerStyle={styles.containerStyle}
@@ -29,15 +29,17 @@ class LibraryItem extends Component {
 
 const styles = StyleSheet.create({
   listStyle: {
-    color: '#1a237e',
-    fontFamily: 'Lato-Regular',
+    color: '#FFFFFF',
+    fontFamily: 'Lato-Bold',
     fontSize: 14,
   },
   subtitleStyle: {
+    color: '#FFFFFF',
     fontFamily: 'Lato-Regular',
     fontSize: 12,
   },
   containerStyle: {
+    backgroundColor: '#1E237E',
     borderWidth: 1,
     borderColor: 'rgba(0, 0, 0, 0.2)',
     margin: 8,
